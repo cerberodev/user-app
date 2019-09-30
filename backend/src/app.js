ee50a5512bedba5699b3b657fc75f208d57eb64a
Pierre Guillen <hi@cerbero.dev>
2019-09-30 17:02:50 -0500

    act

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(require('/routes/user'));
+
+
 module.exports =app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 const app = require('./app');
+const { connect } = require('./database');
 
 async function main(){
-    await app.listen(4000)
+    //Database connected
+    await connect();
+    // Express aplication
+    await app.listen(27017)
     console.log('Server on port 4000');
 }
 
